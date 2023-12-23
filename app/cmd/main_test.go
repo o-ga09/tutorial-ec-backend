5f491f4c32257c52c39dcb80b3a3c754fa0d5c9a
taiti09 <taitiabe1997@gmail.com>
2023-12-23 15:09:33 +0900

    mainのテストはスキップ

--- app/cmd/main_test.go
+++ app/cmd/main_test.go
@@ -1,11 +1,16 @@
 package main
 
-import (
-	"testing"
+import "testing"
 
-	"github.com/stretchr/testify/assert"
-)
-
-func TestMain(t *testing.T) {
-	assert.Equal(t,2,1+1)
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// main関数はスキップ
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
 }
